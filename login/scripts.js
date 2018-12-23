0a022b449d9c9dea8fbddf6ff220a8829a7546c7
Nicolas <ezequielnicolasveron@gmail.com>
2018-12-23 16:37:15 -0300

    actualizacion

--- login/scripts.js
+++ login/scripts.js
@@ -1,9 +1,32 @@
 const BASE_URI = 'https://hcdigital.herokuapp.com';
-const varjwt = document.cookie
-alert (varjwt)
+
 $(document).ready(function(){
+    const jwt = getCookie('jwt');
+    if (jwt !== ''){
+        validarToken(jwt);
+    }
     $('#iniciar').click(iniciarSesion)
+    $('#registrar').click(registrarUsuario)
+   
 });
+
+const validarToken = (token) => {
+
+    fetch(BASE_URI + '/users/me', {
+        method:'GET',
+        headers: {
+            'Authorization': "Bearer " + token,
+
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(data => {
+            if(!data.error){
+//                location.href = "/";
+            }
+        })
+        .catch(error => console.log('error', error))
+}
 
 const iniciarSesion = () => {
     const body = {
@@ -21,7 +44,50 @@ const iniciarSesion = () => {
         }
     })
         .then(data => data.json())
-        .then(data => document.cookie = data.jwt)
+        .then(data => {
+            setCookie('jwt', data.jwt, 2)
+        })
         .catch(error => console.log('error', error))
          
 };
+
+const registrarUsuario = () => {
+    
+    //location.href = "/";
+         
+};
+
+
+function setCookie(cname, cvalue, exdays) {
+    let d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    const expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function checkCookie() {
+    var user = getCookie("username");
+    if (user != "") {
+      alert("Welcome again " + user);
+    } else {
+      user = prompt("Please enter your name:", "");
+      if (user != "" && user != null) {
+        setCookie("username", user, 365);
+      }
+    }
+  }
