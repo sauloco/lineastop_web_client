aa4ef101a430d201d59329a2d56397896c79cb79
Nicolás Verón <ezequielnicolasveron@gmail.com>
2019-01-18 00:59:35 -0300

    fallo en validacion de nombre existente

--- usuario/nuevo/scripts.js
+++ usuario/nuevo/scripts.js
@@ -1,53 +1,26 @@
 $(document).ready(function(){
    
     $('#terminos').prop("checked", false);
-    $('#terminos').click(aceptarTerminos);
+    $('#terminos').click(validarParametros);
    
 });
 
-const aceptarTerminos = () => {
-    if ($('#terminos').prop('checked')){
-        $('#registrar').attr('disabled', false);
-        $('#registrar').click(guardarNuevo);
-    }
-    else {
-        $('#registrar').attr('disabled', true);
-    }
-}
-const guardarNuevo = () => {
-  
-    const params = {
-        username: $('#name').val(),
-        email: $('#email').val(),
-        password: $('#password').val(),
-    };
-
-    getPromise({endpoint: api.auth.local.register, params})
-        .then(response => response.json())
-        .then(data => {
-            if(data.error){
-                return api.common.errorHandler({endpoint: api.auth.local.register, error: data});
-            }
-            M.toast({html:"Revise su correo electrónico. ..sad.asd.ad as.dsa.asd"});
-        })
-        .catch(error => api.common.errorHandler({endpoint: api.auth.local.register, error}));
-};
-    
 const validarParametros = () => {
+
     // TODO: 
-    // validar campos obligatorios 
-    // validar que mail sea valido
-    // validar que contraseña sea mayor o igual a 8 caracteres
-    // validar que checkbox este tildado
+    // **validar campos obligatorios**
+    // **validar que mail sea valido**
+    // **validar que contraseña sea mayor o igual a 8 caracteres**
+    // **validar que checkbox este tildado**
     // validar que usuario no exista ya en la base de datos https://hcdigital.herokuapp.com/users?username={{username}}
     // validar que correo electrónico no exista ya en la base de datos https://hcdigital.herokuapp.com/users?email={{email}}
-   
+    
     const username = $('#name').val();
     if (!username){
         M.toast({html:"Escriba un Nombre de usuario"});
         return false;
     }
-
+    
     const email = $('#email').val();
     if (!email) {
         M.toast({html:"El correo electrónico es obligatorio."});
@@ -61,9 +34,53 @@ const validarParametros = () => {
     }
 
     const password = $('#password').val();
-    if (!password){
-        M.toast({html:"La contraseña es oblligatoria."});
+        if (!password){
+        M.toast({html:"La contraseña es obligatoria."});
         return false;
     }
-    return true;
+    const passtam = password.length;
+    if (passtam!== 8){
+        M.toast({html:"La contraseña debe tener 8 caracteres."});
+        return false;
+    } 
+    
+    if ($('#terminos').prop('checked')){
+        $('#registrar').attr('disabled', false);
+        $('#registrar').click(guardarNuevo);
+    }
+    return;
+    //else {
+      //  $('#registrar').attr('disabled', true);
+    //}
 }
+const guardarNuevo = async() => {
+    
+    const exinom = {username: $('#name').val()}
+    
+    const data = await fetchData({endpoint: api.users.findBy, exinom});
+    console.log(data);
+        if (data.length) { M.toast({html: 'Ya existe un usuario registrado con ese nombre de usuario'})
+        return;
+    }   
+    
+
+    const params = {
+        username: $('#name').val(),
+        email: $('#email').val(),
+        password: $('#password').val()
+    }
+        
+    
+    getPromise({endpoint: api.auth.local.register, params})
+        .then(response => response.json())
+        .then(data => {
+            if(data.error){
+                return api.common.errorHandler({endpoint: api.auth.local.register, error: data});
+            }
+            M.toast({html:"Revise su correo electrónico. ..sad.asd.ad as.dsa.asd"});
+        })
+        .catch(error => api.common.errorHandler({endpoint: api.auth.local.register, error}));
+};
+    
+
+    
