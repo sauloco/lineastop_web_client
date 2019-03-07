2a7f754196d13d0d8bda762ee5b6f32a690528f7
Nicolás Verón <ezequielnicolasveron@gmail.com>
2019-03-07 00:04:12 -0300

    nada aun...

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(()=>{
   $('.sidenav').sidenav();
   iniciarNavBar();
+  collectionsCreator();
   $('#logout').click(logout);
 })
 
@@ -13,3 +14,25 @@ const logout = () => {
   setCookie({name: 'jwt', value: '', day: 0, force: true});
   validarToken();
 }
+
+CONSULTAS = [
+    {
+      persona: {
+        apellido: '',
+        nombre: ''
+      },
+      _id: '',
+      fechaProximaConsulta: ''
+     },
+]
+
+const collectionsCreator = async () => {
+CONSULTAS = await fetchData({endpoint: api.consultas.findBy, params: {persona: PERSONAS[Consulta.ingreseParaBuscar]}});
+  // pagination is the wrapper
+const wrapper = $('.collections');
+$(wrapper).html('');
+ if (!CONSULTAS.length) {
+    $(wrapper).html('Cargue una nueva consulta para esta persona');  
+  } 
+
+//<a href="#! aca pongo la id para mandarla a la pagina de consultas" class="collection-item">mostrar aca objeto persona</a> */
