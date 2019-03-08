5c4cdef2e77c3bd6e8e85083c3adb09460e0a0b9
Nicolás Verón <ezequielnicolasveron@gmail.com>
2019-03-08 17:50:50 -0300

    corregidas identaciones y limpiado el codigo

--- js/menulateral.js
+++ js/menulateral.js
@@ -3,20 +3,16 @@ $(document).ready(() => {
   iniciarNavBar();
   $('#logout').click(logout);
   $('.tooltipped').tooltip();
-  //$('#userTooltip').hover(toast);
 })
-let pepe='';
+let pepe = '';
 const iniciarNavBar = async () => {
   const response = await fetchData({
     endpoint: api.users.me
-    
+
   })
   $('#userTooltip').attr('data-tooltip', response.username);
 }
-/* const toast = () =>{
-  M.toast({html: `${pepe}`});
 
-} */
 const logout = () => {
   setCookie({
     name: 'jwt',
